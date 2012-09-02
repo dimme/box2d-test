db8bd3d22958b1acefa28c668a0e261f586b0930
Dimitrios Vlastaras <mail@dimme.net>
2012-09-02 20:06:32 +0200

    Initial Commit

---
+++ Box2DTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Box2DTest
+//
+//  Created by Dimitrios Vlastaras on 2012-09-02.
+//  Copyright (c) 2012 Dimitrios Vlastaras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Box2DTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Box2DTest
+//
+//  Created by Dimitrios Vlastaras on 2012-09-02.
+//  Copyright (c) 2012 Dimitrios Vlastaras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
